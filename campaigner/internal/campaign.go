cc7a7a3ca0b040813292d7f2852fdf13f5fd1990
Nicholas Ong <nicholasongck@gmail.com>
2023-01-24 20:55:08 +0800

    feat: create and read for campaigns

--- campaigner/internal/campaign.go
+++ campaigner/internal/campaign.go
@@ -4,14 +4,40 @@ import (
 	"github.com/cs301-itsa/project-2022-23t2-g1-t7/campaigner/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	// "io/ioutil"
 )
 
 // GetCampaigns - GET /campaign
 // Get all campaigns
 func GetCampaigns(c *gin.Context) {
 	var campaigns []models.Campaign
-
 	models.DB.Find(&campaigns)
-
 	c.JSON(http.StatusOK, gin.H{"data": campaigns})
+}
+
+func GetCampaignById(c *gin.Context){
+	var campaign models.Campaign
+	id := c.Param("id")
+	models.DB.Where("id = ?",id).Find(&campaign)
+	c.JSON(http.StatusOK, gin.H{"data":campaign})
+}
+
+func CreateCampaign(c *gin.Context){
+	//TODO: Error Checking for mandatory fields
+	var campaignBody models.Campaign
+	if err := c.BindJSON(&campaignBody); err != nil {
+        c.JSON(400, gin.H{"error": err.Error()})
+        return
+    }
+	err:= models.DB.Create(&campaignBody).Error
+
+	if  (err == nil){
+		c.JSON(200, gin.H{"message": "Success"})
+		return
+	}
+
+	//TODO: Change to more descriptive error 
+	//(Not sure what scenario it will end up here)
+	c.JSON(500, gin.H{"error":err.Error()})
+	return
 }
--- campaigner/models/campaign.go
+++ campaigner/models/campaign.go
@@ -3,15 +3,25 @@ package models
 import (
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"time"
 )
 
 type Campaign struct {
 	ID uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;<-:create"`
+	MinSpend float64 `json:"minSpend"`
+	Start time.Time `json:"startDate`
+	Duration time.Time `json:"endDate"`
+	RewardProgram string `json:"rewardProgram"`
+	RewardAmount int `json:"rewardAmount"` 
 	// add other fields here accordingly
 
 }
 
 func (campaign *Campaign) BeforeCreate(tx *gorm.DB) (err error) {
 	campaign.ID = uuid.New()
+
+	// To remove when we decide how we taking in date params
+	campaign.Start = time.Now()
+	campaign.Duration = time.Now()
 	return
 }
