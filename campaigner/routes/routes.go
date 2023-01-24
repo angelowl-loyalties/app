63501197697535c34ea1e433e0394e48cf477482
Nicholas Ong <nicholasongck@gmail.com>
2023-01-24 20:55:08 +0800

    feat: create and read for campaigns

--- campaigner/routes/routes.go
+++ campaigner/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/cs301-itsa/project-2022-23t2-g1-t7/campaigner/internal"
 )
 
 // PingExample godoc
@@ -21,6 +22,10 @@ func Health(c *gin.Context) {
 func InitialiseRoutes(router *gin.Engine) {
 	router.GET("/health", Health)
 
-	//campaign := router.Group("/campaign")
+	campaign := router.Group("/campaign")
+	campaign.POST("/",internal.CreateCampaign)
+	campaign.GET("/",internal.GetCampaigns)
+	campaign.GET("/:id",internal.GetCampaignById)
+
 	// add your other routes accordingly
 }
