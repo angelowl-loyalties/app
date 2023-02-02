595e756dfbe0915da3d9e8a671a4900d24c93b66
Lye Jian Yi <lyejianyi@gmail.com>
2023-02-02 21:44:44 +0800

    fixed port number and added cassandra datatype comment

--- informer/models/transaction.go
+++ informer/models/transaction.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Transaction struct {
-	ID              gocql.UUID `json:"ID"`
-	CardID          gocql.UUID `json:"CardID"`
-	Merchant        string     `json:"Merchant"`
-	MCC             int        `json:"MCC"`
-	Currency        string     `json:"Currency"`
-	Amount          float64    `json:"Amount"`
-	SGDAmount       float64    `json:"SGDAmount"`
-	TransactionID   string     `json:"TransactionID"`
-	TransactionDate string     `json:"TransactionDate"`
-	CardPAN         string     `json:"CardPAN"`
-	CardType        string     `json:"CardType"`
+	ID              gocql.UUID `json:"ID"` //cassandra uuid
+	CardID          gocql.UUID `json:"CardID"` //cassandra uuid
+	Merchant        string     `json:"Merchant"` //cassandra text
+	MCC             int        `json:"MCC"` //cassandra int
+	Currency        string     `json:"Currency"` //cassandra text
+	Amount          float64    `json:"Amount"` //cassandra double
+	SGDAmount       float64    `json:"SGDAmount"` //cassandra double
+	TransactionID   string     `json:"TransactionID"` //cassandra text
+	TransactionDate string     `json:"TransactionDate"` //cassandra text
+	CardPAN         string     `json:"CardPAN"` //cassandra text
+	CardType        string     `json:"CardType"` //cassandra text
 }
