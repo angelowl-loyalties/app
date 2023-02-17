cc9ead089a63c45431e359fea779e0a0fe1f590f
Justin <justin.100600@gmail.com>
2023-02-18 03:06:23 +0800

    chore: json tag models

--- rewarder/models/exclusion.go
+++ rewarder/models/exclusion.go
@@ -7,30 +7,7 @@ import (
 )
 
 type Exclusion struct {
-	ID        uuid.UUID
-	MCC       int
-	ValidFrom time.Time
-}
-
-var Seed_exclusions = []Exclusion{
-	{
-		ID:        uuid.MustParse("e5e2a5c5-f6e0-48e7-9ccb-7c6cfa78a873"),
-		MCC:       5915,
-		ValidFrom: time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
-	},
-	{
-		ID:        uuid.MustParse("6ddb0ecc-0f05-44c2-b6c3-7587d5a56bab"),
-		MCC:       7011,
-		ValidFrom: time.Date(2023, 2, 1, 0, 0, 0, 0, time.UTC),
-	},
-	{
-		ID:        uuid.MustParse("1e5b5fd5-69d5-4f98-8f33-1b9069d31a5b"),
-		MCC:       5963,
-		ValidFrom: time.Date(2023, 3, 1, 0, 0, 0, 0, time.UTC),
-	},
-	{
-		ID:        uuid.MustParse("e38adb10-a96a-4b55-aebd-7cdc9b973e7b"),
-		MCC:       5977,
-		ValidFrom: time.Date(2023, 4, 1, 0, 0, 0, 0, time.UTC),
-	},
+	ID        uuid.UUID `json:"id"`
+	MCC       int       `json:"mcc"`
+	ValidFrom time.Time `json:"valid_from"`
 }
--- rewarder/models/transaction.go
+++ rewarder/models/transaction.go
@@ -15,17 +15,3 @@ type Transaction struct {
 	CardPAN         string    `json:"card_pan"`
 	CardType        string    `json:"card_type"`
 }
-
-var Seed_transaction = Transaction{
-	ID:              uuid.MustParse("4aab2f7c-4dd3-4a77-beb8-8582048c9bdb"),
-	CardID:          uuid.MustParse("3c0b3d7f-c011-4a7d-b47e-1f7c03a8ca53"),
-	Merchant:        "Acme Inc.",
-	MCC:             5912,
-	Currency:        "SGD",
-	Amount:          100.00,
-	SGDAmount:       142.40,
-	TransactionID:   "1234abcd",
-	TransactionDate: "10-02-23",
-	CardPAN:         "1234567890123456",
-	CardType:        "Visa",
-}
