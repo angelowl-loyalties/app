aef5983318827153fe010f470993f1a867969585
Justin <justin.100600@gmail.com>
2023-03-02 22:55:27 +0800

    feat: setup lambda boilerplate

--- authorizer/main.go
+++ authorizer/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type response struct {
+	UTC time.Time `json:"utc"`
+}
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	now := time.Now()
+	resp := &response{
+		UTC: now.UTC(),
+	}
+	body, err := json.Marshal(resp)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	lambda.Start(handleRequest)
 }
