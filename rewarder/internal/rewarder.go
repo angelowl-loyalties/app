1f9638e4d8ba855eaf6aeb25cc3472e6a9e0520d
Nicholas Ong <nicholasongck@gmail.com>
2023-03-06 22:03:15 +0800

    feat: individual processing of base and campaign

--- rewarder/internal/rewarder.go
+++ rewarder/internal/rewarder.go
@@ -33,7 +33,7 @@ func ProcessMessage(transaction models.Transaction) error {
 
 	// TODO: Proper Error handling
 	if err != nil {
-		// log.Fatalln(err)
+		log.Fatalln(err)
 		return nil //TODO: Fix this to do something when date couldnt be parsed
 	}
 
@@ -56,6 +56,7 @@ func ProcessMessage(transaction models.Transaction) error {
 			RewardAmount:    delta,
 			Remarks:         remarks,
 		}
+
 		err := models.RewardCreate(reward)
 		// TODO: Proper Error handling
 		if err != nil {
@@ -66,69 +67,55 @@ func ProcessMessage(transaction models.Transaction) error {
 	} else {
 		campaigns := getMatchingCampaigns(transaction)
 
-		if campaigns != nil {
-			delta := 0.0
-			remarks := ""
-			for _, campaign := range campaigns {
-				tempDelta := calculateDeltaType(campaign.RewardAmount, transaction.Amount)
-				if tempDelta > delta {
-					delta = tempDelta
-					remarks = campaign.Name + " applied" //TODO: Format this string to appropriate description
-				}
-			}
+		baseDelta := 0.0
+		promoDelta := 0.0
+		remarks := ""
+		baseMatchedCampaigns := campaigns[0]
+		promoMatchedCampaigns := campaigns[1]
 
-			reward := models.Reward{
-				ID:              gocql.UUID(transaction.ID),
-				CardID:          gocql.UUID(transaction.CardID),
-				Merchant:        transaction.Merchant,
-				MCC:             transaction.MCC,
-				Currency:        transaction.Currency,
-				Amount:          transaction.Amount,
-				SGDAmount:       transaction.SGDAmount,
-				TransactionID:   transaction.TransactionID,
-				TransactionDate: transaction.TransactionDate,
-				CardPAN:         transaction.CardPAN,
-				CardType:        transaction.CardType,
-				RewardAmount:    delta,
-				Remarks:         remarks,
+		//TODO: Support multiple currencies other than USD
+		if transaction.Currency != "SGD" {
+			transaction.Amount = convertToSGD(transaction.Amount)
+		}
+		for _, campaign := range baseMatchedCampaigns {
+
+			tempDelta := calculateDeltaType(campaign.RewardAmount, transaction.Amount)
+			if tempDelta > baseDelta {
+				baseDelta = tempDelta
+				remarks = campaign.Name + " applied" //TODO: Format this string to appropriate description
 			}
-			err := models.RewardCreate(reward)
-			// TODO: Proper Error handling
-			if err != nil {
-				log.Fatalln(err)
-				return err
-			}
-		} else {
-			//Should never reach here
-			log.Fatalln("No matching campaigns found")
 		}
 
-		// } else {
-		// 	delta := 69.0
-		// 	remarks := "Base campaign applied"
+		for _, campaign := range promoMatchedCampaigns {
 
-		// 	reward := models.Reward{
-		// 		ID:              gocql.UUID(transaction.ID),
-		// 		CardID:          gocql.UUID(transaction.CardID),
-		// 		Merchant:        transaction.Merchant,
-		// 		MCC:             transaction.MCC,
-		// 		Currency:        transaction.Currency,
-		// 		Amount:          transaction.Amount,
-		// 		SGDAmount:       transaction.SGDAmount,
-		// 		TransactionID:   transaction.TransactionID,
-		// 		TransactionDate: transaction.TransactionDate,
-		// 		CardPAN:         transaction.CardPAN,
-		// 		CardType:        transaction.CardType,
-		// 		RewardAmount:    delta,
-		// 		Remarks:         remarks,
-		// 	}
-		// 	err := models.RewardCreate(reward)
-		// 	// TODO: Proper Error handling
-		// 	if err != nil {
-		// 		log.Fatalln(err)
-		// 		return err
-		// 	}
-		// }
+			tempDelta := calculateDeltaType(campaign.RewardAmount, transaction.Amount)
+			if tempDelta > promoDelta {
+				promoDelta = tempDelta
+				remarks = campaign.Name + " applied" //TODO: Format this string to appropriate description
+			}
+		}
+
+		reward := models.Reward{
+			ID:              gocql.UUID(transaction.ID),
+			CardID:          gocql.UUID(transaction.CardID),
+			Merchant:        transaction.Merchant,
+			MCC:             transaction.MCC,
+			Currency:        transaction.Currency,
+			Amount:          transaction.Amount,
+			SGDAmount:       transaction.SGDAmount,
+			TransactionID:   transaction.TransactionID,
+			TransactionDate: transaction.TransactionDate,
+			CardPAN:         transaction.CardPAN,
+			CardType:        transaction.CardType,
+			RewardAmount:    baseDelta + promoDelta,
+			Remarks:         remarks,
+		}
+		err := models.RewardCreate(reward)
+		// TODO: Proper Error handling
+		if err != nil {
+			log.Fatalln(err)
+			return err
+		}
 	}
 	return nil
 }
@@ -144,29 +131,36 @@ func isExcluded(transactionDate time.Time, mcc int) bool {
 	return false
 }
 
-func getMatchingCampaigns(transaction models.Transaction) (campaign []*models.Campaign) {
+func getMatchingCampaigns(transaction models.Transaction) (campaign [][]models.Campaign) {
+	//Returns a 2D array of campaigns, [ [BaseMatchedCampaigns], [PromoMatchedCampaigns] ]
+
 	// TODO: start using the transactions start date instead of time now
-	// transactionDate, _ := time.Parse(YYYYMMDD, transactionDateStr)
-	var matchingCampaigns []*models.Campaign
+	promoMatchingCampaigns := []models.Campaign{}
+	baseMatchingCampaigns := []models.Campaign{}
+
+	resultMatchingCampaigns := [][]models.Campaign{}
 	for _, campaign := range BaseCampaignsEtcd {
+
 		if isCampaignMatch(&campaign, &transaction) {
-			matchingCampaigns = append(matchingCampaigns, &campaign)
+			baseMatchingCampaigns = append(baseMatchingCampaigns, campaign)
 		}
 	}
 
 	for _, campaign := range PromoCampaignsEtcd {
 		if isCampaignMatch(&campaign, &transaction) {
-			matchingCampaigns = append(matchingCampaigns, &campaign)
+			promoMatchingCampaigns = append(promoMatchingCampaigns, campaign)
 		}
 	}
-	return matchingCampaigns
+
+	resultMatchingCampaigns = append(resultMatchingCampaigns, baseMatchingCampaigns)
+	resultMatchingCampaigns = append(resultMatchingCampaigns, promoMatchingCampaigns)
+	return resultMatchingCampaigns
 }
 
 func isCampaignMatch(campaign *models.Campaign, transaction *models.Transaction) bool {
 	if campaign.RewardProgram != transaction.CardType {
 		return false
 	}
-
 	//TODO: Change to proper transaction date
 	if !campaign.Start.Before(time.Now()) || !campaign.End.After(time.Now()) || campaign.MinSpend > transaction.Amount {
 		return false
@@ -177,12 +171,14 @@ func isCampaignMatch(campaign *models.Campaign, transaction *models.Transaction)
 	if campaign.Merchant != "" && strings.Contains(transaction.Merchant, campaign.Merchant) {
 		return false
 	}
+	if campaign.MinSpend > transaction.Amount {
+		return false
+	}
 	return true
 }
 
 func calculateDeltaType(rewardAmount int, spentAmount float64) float64 {
-	// TODO: include other logic here
-	// Min spend
+
 	return float64(rewardAmount) * spentAmount
 }
 
--- rewarder/models/transaction.go
+++ rewarder/models/transaction.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 type Transaction struct {
 	ID              uuid.UUID `json:"id"`
@@ -15,3 +17,94 @@ type Transaction struct {
 	CardPAN         string    `json:"card_pan"`
 	CardType        string    `json:"card_type"`
 }
+
+// var Seed_campaigns = []Campaign{
+// 	{
+// 		ID:                 uuid.MustParse("7b1f04eb-f54c-4f9d-8baf-a4c00dddf3b3"),
+// 		Name:               "Summer Sale",
+// 		MinSpend:           50.0,
+// 		Start:              time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+// 		End:                time.Date(2023, 8, 31, 23, 59, 59, 0, time.UTC),
+// 		RewardProgram:      "Points",
+// 		RewardAmount:       500,
+// 		MCC:                7011,
+// 		Merchant:           "Best Buy",
+// 		ForForeignCurrency: true,
+// 	},
+// 	{
+// 		ID:                 uuid.MustParse("1c7f6942-85f9-4a9a-b1ab-6dab27c94f83"),
+// 		Name:               "Winter Warmup",
+// 		MinSpend:           100.0,
+// 		Start:              time.Date(2023, 12, 1, 0, 0, 0, 0, time.UTC),
+// 		End:                time.Date(2024, 2, 28, 23, 59, 59, 0, time.UTC),
+// 		RewardProgram:      "Cashback",
+// 		RewardAmount:       25,
+// 		MCC:                5913,
+// 		ForForeignCurrency: true,
+// 	},
+// 	{
+// 		ID:                 uuid.MustParse("ddb0a58f-6dca-41f3-a3a9-d40961670b5b"),
+// 		Name:               "Spring Fling",
+// 		MinSpend:           0.0,
+// 		Start:              time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC),
+// 		End:                time.Date(2024, 5, 31, 23, 59, 59, 0, time.UTC),
+// 		RewardProgram:      "Visa",
+// 		RewardAmount:       300,
+// 		MCC:                5963,
+// 		ForForeignCurrency: true,
+// 		Merchant:           "",
+// 	},
+// }
+
+// var Seed_BaseCampaign = []Campaign{
+// 	//Base Campaign
+// 	{
+// 		ID:                 uuid.MustParse("ddb0a58f-6dca-41f3-a3a9-d40961670c5b"),
+// 		Name:               "Base Campaign",
+// 		MinSpend:           0.0,
+// 		Start:              time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+// 		End:                time.Date(2025, 5, 31, 23, 59, 59, 0, time.UTC),
+// 		RewardProgram:      "Visa",
+// 		RewardAmount:       10,
+// 		MCC:                5963,
+// 		ForForeignCurrency: false,
+// 		Merchant:           "",
+// 	},
+// }
+
+// var Seed_exclusions = []Exclusion{
+// 	{
+// 		ID:        uuid.MustParse("e5e2a5c5-f6e0-48e7-9ccb-7c6cfa78a873"),
+// 		MCC:       5915,
+// 		ValidFrom: time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+// 	},
+// 	{
+// 		ID:        uuid.MustParse("6ddb0ecc-0f05-44c2-b6c3-7587d5a56bab"),
+// 		MCC:       7011,
+// 		ValidFrom: time.Date(2023, 2, 1, 0, 0, 0, 0, time.UTC),
+// 	},
+// 	{
+// 		ID:        uuid.MustParse("1e5b5fd5-69d5-4f98-8f33-1b9069d31a5b"),
+// 		MCC:       5963,
+// 		ValidFrom: time.Date(2023, 3, 1, 0, 0, 0, 0, time.UTC),
+// 	},
+// 	{
+// 		ID:        uuid.MustParse("e38adb10-a96a-4b55-aebd-7cdc9b973e7b"),
+// 		MCC:       5977,
+// 		ValidFrom: time.Date(2023, 4, 1, 0, 0, 0, 0, time.UTC),
+// 	},
+// }
+
+// var Seed_transaction = Transaction{
+// 	ID:              uuid.MustParse("4aab2f7c-4dd3-4a77-beb8-8582048c9bdb"),
+// 	CardID:          uuid.MustParse("3c0b3d7f-c011-4a7d-b47e-1f7c03a8ca53"),
+// 	Merchant:        "Acme Inc.",
+// 	MCC:             5912,
+// 	Currency:        "USD",
+// 	Amount:          100.00,
+// 	SGDAmount:       142.40,
+// 	TransactionID:   "1234abcd",
+// 	TransactionDate: "2023-10-23",
+// 	CardPAN:         "1234567890123456",
+// 	CardType:        "Visa",
+// }
