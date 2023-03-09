e008e63f36a8327ce5735591a4086c150747e8f0
Wen Liang Goh <wlgoh.2020@scis.smu.edu.sg>
2023-03-10 03:40:56 +0800

    infra: Database for profiler

--- .infra/database.tf
+++ .infra/database.tf
@@ -30,3 +30,26 @@ resource "aws_db_instance" "campaignex_db" {
 
   skip_final_snapshot = true
 }
+
+resource "aws_db_instance" "profiler_db" {
+  allocated_storage = 20
+  storage_type      = "gp3"
+
+  db_subnet_group_name = aws_db_subnet_group.database.name
+  vpc_security_group_ids = [
+    aws_security_group.angelowl_postgres.id
+  ]
+
+  identifier = "profiler-db"
+  db_name    = "profiler"
+  engine     = "postgres"
+
+  engine_version = "14.6"
+  instance_class = "db.t3.micro"
+  multi_az       = true
+
+  username = "postgresAdmin"
+  password = "pgDefaultPwdToChange!"
+
+  skip_final_snapshot = true
+}
