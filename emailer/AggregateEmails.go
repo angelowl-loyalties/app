b3c878875116ebf372ff83271e128deb245e3aca
omerwyo <omerbaggia123@gmail.com>
2023-03-12 23:22:29 +0800

    feat: emailer functionality

--- emailer/AggregateEmails.go
+++ emailer/AggregateEmails.go
@@ -1,41 +1,61 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"os"
 	"strconv"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/gocql/gocql"
 	"github.com/google/uuid"
-	"github.com/segmentio/kafka-go"
+	"github.com/scylladb/gocqlx"
+	"github.com/scylladb/gocqlx/qb"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-const (
-	DDMMYY   = "02-01-06"
-	YYYYMMDD = "2006-01-02"
-)
+type Reward struct {
+	ID              gocql.UUID `json:"id"`               // cassandra uuid
+	CardID          gocql.UUID `json:"card_id"`          // cassandra uuid
+	Merchant        string     `json:"merchant"`         // cassandra text
+	MCC             int        `json:"mcc"`              // cassandra int
+	Currency        string     `json:"currency"`         // cassandra text
+	Amount          float64    `json:"amount"`           // cassandra double
+	SGDAmount       float64    `json:"sgd_amount"`       // cassandra double
+	TransactionID   string     `json:"transaction_id"`   // cassandra text
+	TransactionDate string     `json:"transaction_date"` // cassandra text
+	CardPAN         string     `json:"card_pan"`         // cassandra text
+	CardType        string     `json:"card_type"`        // cassandra text
+	RewardAmount    float64    `json:"reward_amount"`    // cassandra double
+	Remarks         string     `json:"remarks"`          // cassandra text
+}
 
-type Transaction struct {
-	ID              uuid.UUID `json:"id"`
-	CardID          uuid.UUID `json:"card_id"`
-	Merchant        string    `json:"merchant"`
-	MCC             int       `json:"mcc"`
-	Currency        string    `json:"currency"`
-	Amount          float64   `json:"amount"`
-	SGDAmount       float64   `json:"sgd_amount"`
-	TransactionID   string    `json:"transaction_id"`
-	TransactionDate string    `json:"transaction_date"`
-	CardPAN         string    `json:"card_pan"`
-	CardType        string    `json:"card_type"`
+type Card struct {
+	ID               uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;<-:create"`
+	CardPan          string    `json:"card_pan" gorm:"unique;not null" binding:"required,credit_card"`
+	UserID           uuid.UUID `json:"user_id" gorm:"type:uuid" binding:"required"` // card belongs to one user
+	CardTypeCardType string    `json:"card_type" binding:"required"`                // card belongs to one card type
+}
+
+type User struct {
+	ID          uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;<-:create"`
+	FirstName   string    `json:"first_name" gorm:"type:varchar(255);not null"`
+	LastName    string    `json:"last_name" gorm:"type:varchar(255);not null"`
+	Phone       string    `json:"phone" gorm:"not null"`
+	Email       string    `json:"email" gorm:"unique;not null"`
+	Password    string    `json:"-" gorm:"not null"`
+	Role        string    `gorm:"type:varchar(255);not null"`
+	CreditCards []Card    // one user has many credit cards
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 type S3Event struct {
@@ -48,103 +68,239 @@ type S3Event struct {
 	} `json:"Records"`
 }
 
-// Follows FTP ordering
-func (transaction *Transaction) Parse(transactionCsv []string) error {
-	transaction.ID = uuid.MustParse(transactionCsv[0])
-	transaction.TransactionID = transactionCsv[1]
-	transaction.Merchant = transactionCsv[2]
-	transaction.MCC, _ = strconv.Atoi(transactionCsv[3])
-	transaction.Currency = transactionCsv[4]
-	transaction.Amount, _ = strconv.ParseFloat(transactionCsv[5], 64)
+var Cassandra *gocql.Session
+var Postgres *gorm.DB
+var svc *ses.SES
 
-	// transaction.SGDAmount, _ = strconv.ParseFloat(transactionCsv[6], 64)
-	transaction.SGDAmount = 0.0
+const YYYYMMDD = "2006-01-02"
 
-	// Parse time and catch errors from homework format
-	// tempDate, err := time.Parse(DDMMYY, transactionCsv[6])
-	// if err != nil {
-	// 	return err
-	// }
+func ConnectCassandra(dbHost, dbPort, username, password, keyspace string, useSSL bool) {
+	cluster := gocql.NewCluster(dbHost)
+	cluster.Keyspace = keyspace
+	cluster.Consistency = gocql.LocalQuorum
 
-	// Format time into ISO format
-	// transaction.TransactionDate = tempDate.Format(YYYYMMDD)
+	dbPortInt, err := strconv.Atoi(dbPort)
+	if err == nil {
+		cluster.Port = dbPortInt
+	}
 
-	transaction.TransactionDate = transactionCsv[6]
-	transaction.CardID = uuid.MustParse(transactionCsv[7])
-	transaction.CardPAN = transactionCsv[8]
-	transaction.CardType = transactionCsv[9]
+	cluster.Authenticator = gocql.PasswordAuthenticator{
+		Username: username,
+		Password: password,
+	}
 
+	if useSSL {
+		cluster.SslOpts = &gocql.SslOptions{
+			CaPath: "./sf-class2-root.crt",
+		}
+	}
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Connected to Rewards Cassandra DB")
+	Cassandra = session
+}
+
+func ConnectPostgres(dbConnString string) {
+	db, err := gorm.Open(postgres.Open(dbConnString), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Connected to Profiler Postgres")
+	Postgres = db
+}
+
+func CreateSESSession() {
+	// Create a new session in the ap-southeast-1 region.
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("ap-southeast-1")},
+	)
+
+	// Create an SES session.
+	svc = ses.New(sess)
+}
+
+func GetTodaysRewards() (rewards []Reward, _ error) {
+	todaysDate := time.Now().Format(YYYYMMDD)
+
+	todaysDateLiteral := fmt.Sprintf("'%s'", todaysDate)
+
+	//TODO: Replace todaysDate with a date in the csv data from ftp server, since no rewards with todaysdate
+
+	// Equivalent to the following query
+	// select * from transactions.rewards where transaction_date == {today's date} and reward_amount > 0 ALLOW FILTERING;
+	stmt, _ := qb.Select("transactions.rewards").Where(qb.EqLit("transaction_date", todaysDateLiteral)).Where(qb.GtLit("reward_amount", "0")).AllowFiltering().ToCql()
+
+	err := gocqlx.Select(&rewards, Cassandra.Query(stmt))
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return rewards, nil
+}
+
+func GetUniqueCardIds(rewards []Reward) []uuid.UUID {
+	// Create a map to store the unique card IDs
+	uniqueCardIds := make(map[uuid.UUID]bool)
+
+	// Iterate over the rewards and add the card IDs to the map
+	for _, reward := range rewards {
+		card_uuid := uuid.MustParse(reward.CardID.String())
+		uniqueCardIds[card_uuid] = true
+	}
+
+	// Convert the map keys to an array
+	cardIds := make([]uuid.UUID, 0, len(uniqueCardIds))
+	for cardId := range uniqueCardIds {
+		cardIds = append(cardIds, cardId)
+	}
+
+	return cardIds
+}
+
+func GetRewardsByEmailAndCardID(rewards []Reward, cardIDs []uuid.UUID) (map[string]map[uuid.UUID][]Reward, error) {
+	// Fetch user emails and card IDs associated with the provided card IDs
+	type CardUserDetails struct {
+		CardID uuid.UUID
+		UserID uuid.UUID
+		Email  string
+	}
+	var cardUserDetails []CardUserDetails
+	if err := Postgres.Table("cards").
+		Select("cards.id AS card_id, users.id AS user_id, users.email").
+		Joins("JOIN users ON users.id = cards.user_id").
+		Where("cards.id IN (?)", cardIDs).
+		Scan(&cardUserDetails).Error; err != nil {
+		return nil, err
+	}
+
+	emailByCardId := make(map[uuid.UUID]string)
+
+	for _, cardUser := range cardUserDetails {
+		emailByCardId[cardUser.CardID] = cardUser.Email
+	}
+
+	rewardsByEmailAndCardID := make(map[string]map[uuid.UUID][]Reward)
+
+	// Assemble a map
+	// Where the key is a user's email and the value is a map
+	// That map has the keys of cardIds associated with that user, and an array of rewards for the day
+	// All content that appears on this map are rewards that have been applied today and have a reward_amount > 0
+	for _, reward := range rewards {
+		rewardCardId := uuid.MustParse(reward.CardID.String())
+		email := emailByCardId[rewardCardId]
+
+		if rewardsByEmailAndCardID[email] == nil {
+			rewardsByEmailAndCardID[email] = make(map[uuid.UUID][]Reward)
+			rewardsByEmailAndCardID[email][rewardCardId] = []Reward{reward}
+		} else {
+			if rewardsByEmailAndCardID[email][rewardCardId] == nil {
+				rewardsByEmailAndCardID[email][rewardCardId] = []Reward{reward}
+			} else {
+				rewardsByEmailAndCardID[email][rewardCardId] = append(rewardsByEmailAndCardID[email][rewardCardId], reward)
+			}
+		}
+	}
+
+	return rewardsByEmailAndCardID, nil
+}
+
+func SendEmail(recipient string, cardRewards map[uuid.UUID][]Reward) error {
+	// Assemble the body of the email
+	// TODO: Replace with this with a pretty template
+	htmlBody := ""
+
+	for cardId, rewards := range cardRewards {
+		htmlBody += "Reward For Card ID: " + cardId.String()
+		for _, reward := range rewards {
+			rewardStr, err := json.Marshal(reward)
+			if err != nil {
+				fmt.Println("JSON parse reward failed")
+			}
+			htmlBody += string(rewardStr) + "\n"
+		}
+		htmlBody += "\n"
+	}
+
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(recipient),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String(htmlBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String("UTF-8"),
+				Data:    aws.String("Your AngelOwl rewards accumulated today"),
+			},
+		},
+		Source: aws.String("noreply@itsag1t2.com"),
+		// Uncomment to use a configuration set
+		// ConfigurationSetName: aws.String(ConfigurationSet),
+	}
+
+	// Attempt to send the email.
+	result, err := svc.SendEmail(input)
+
+	// Display error messages if they occur.
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return err
+	}
+
+	fmt.Println("Email Sent to address: " + recipient)
+	fmt.Println(result)
 	return nil
 }
 
 func HandleRequest(ctx context.Context, event S3Event) (string, error) {
-	// Establish an AWS session
-	sess := session.Must(session.NewSession())
-	s3Svc := s3.New(sess)
+	os.Getenv("CASSANDRA_CONN_STRING")
+	dbHost := os.Getenv("CASSANDRA_CONN_STRING")
+	dbPort := os.Getenv("CASSANDRA_PORT")
+	dbKeyspace := os.Getenv("CASSANDRA_PORT")
+	dbUser := os.Getenv("CASSANDRA_USERNAME")
+	dbPass := os.Getenv("CASSANDRA_PASSWORD")
+	dbUseSSL := true
+	ConnectCassandra(dbHost, dbPort, dbUser, dbPass, dbKeyspace, dbUseSSL)
 
-	// Define the S3 bucket and file key
-	bucket := "angel-owl-spendfiles"
-	fileKey := "bigspend.csv"
+	dbConnString := os.Getenv("POSTGRES_CONN_STRING")
+	ConnectPostgres(dbConnString)
 
-	// Download the file from S3
-	result, err := s3Svc.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(fileKey),
-	})
-	if err != nil {
-		fmt.Printf("Error getting object from S3: %v\n", err)
-		os.Exit(1)
-	}
+	CreateSESSession()
 
-	// Read the contents of the file
-	reader := csv.NewReader(bufio.NewReader(result.Body))
+	rewards, err := GetTodaysRewards()
 
-	_, err = reader.Read()
+	cards := GetUniqueCardIds(rewards)
 
-	if err != nil {
-		fmt.Printf("Error reading header: %v\n", err)
-		os.Exit(1)
-	}
+	mailMap, err := GetRewardsByEmailAndCardID(rewards, cards)
 
-	producer := kafka.NewWriter(kafka.WriterConfig{
-		Brokers:      []string{"angelowlmsk.aznt6t.c3.kafka.ap-southeast-1.amazonaws.com:9092"},
-		Topic:        "transaction",
-		Balancer:     &kafka.LeastBytes{},
-		BatchSize:    1000,
-		BatchTimeout: 100 * time.Millisecond,
-	})
-	defer producer.Close()
-
-	for {
-		record, err := reader.Read()
-
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Printf("Error reading record from .csv file: %v", err)
-		}
-
-		var m Transaction
-		err = m.Parse(record)
-		if err != nil {
-			fmt.Printf("Error parsing transaction: %v", err)
-		}
-
-		b, err := json.Marshal(m)
-		if err != nil {
-			fmt.Printf("Error marshaling JSON: %v", err)
-		}
-
-		go func() {
-			err := producer.WriteMessages(ctx, kafka.Message{
-				Key:   []byte("transaction6"),
-				Value: []byte(b),
-			})
-			if err != nil {
-				fmt.Printf("Error writing to Producer: %v", err)
-			}
-		}()
+	for email, cardRewards := range mailMap {
+		err = SendEmail(email, cardRewards)
 	}
 
 	return "", err
---
+++ emailer/go.mod
@@ -0,0 +1,28 @@
+module main
+
+go 1.19
+
+require (
+	github.com/aws/aws-lambda-go v1.38.0
+	github.com/aws/aws-sdk-go v1.44.219
+	github.com/gocql/gocql v1.3.1
+	github.com/google/uuid v1.3.0
+	github.com/scylladb/gocqlx v1.5.0
+	gorm.io/driver/postgres v1.5.0
+	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11
+)
+
+require (
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/scylladb/go-reflectx v1.0.1 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+)
