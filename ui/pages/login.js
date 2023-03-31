0bf8c016aa94108c7d57d50efdaf447e3d585dd8
Alvin Owyong <alvinowyong@gmail.com>
2023-03-31 15:49:07 +0800

    chore: optimsation

--- ui/pages/login.js
+++ ui/pages/login.js
@@ -15,6 +15,7 @@ import {
     Link,
     Stack,
 } from '@chakra-ui/react';
+import Loading from './loading';
 import { getCsrfToken, signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useEffect } from 'react';
@@ -29,6 +30,12 @@ export default function Login() {
     const password = useRef();
     const [showPassword, setShowPassword] = useState(false);
     const handleShowClick = () => setShowPassword(!showPassword);
+    const { data: session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push("/login")
+        }
+    });
 
     const handleLogin = async (e) => {
         setLoading(true)
@@ -50,17 +57,22 @@ export default function Login() {
     };
 
     useEffect(() => {
+        if (session) {
+            router.push("/")
+        }
         document.title = "Log in | Ascenda"
-    }, [])
+    }, [session])
 
     return (
+        <>
+        {loading ? <Loading /> :
         <Flex
             flexDirection="column"
             width="100wh"
             height="100vh"
             justifyContent="center"
             alignItems="center"
-            bgImage="url('/bg.jpg')"
+            bgImage="https://ik.imagekit.io/alvinowyong/g1t2/bg.webp?tr=w-1920,h-1080"
             bgPosition="center"
             bgRepeat="no-repeat"
             bgSize="cover"
@@ -74,13 +86,21 @@ export default function Login() {
                 shadow="md"
                 bg={'gray.50'}
                 rounded={'xl'}
-                p={3}
+                p={5}
                 height="-webkit-fit-content"
                 spacing={{ base: 8 }}
                 maxW={{ lg: 'md' }}
             >
                 <Box pt={8}>
-                    <Image priority={true} src="/ascenda.webp" width='0' height='0' sizes="100vw" style={{ width: '150px', height: 'auto' }} alt="ascenda logo" />
+                <img
+							priority={true}
+							src="https://ik.imagekit.io/alvinowyong/g1t2/ascenda.webp"
+							width="0"
+							height="0"
+							sizes="100vw"
+							style={{ width: "150px", height: "auto" }}
+							alt="logo"
+						/>
                 </Box>
                 <Box minW={{ base: "90%", md: "100%" }}>
                     <form>
@@ -89,14 +109,14 @@ export default function Login() {
                             p="1rem"
                         >
                             <FormControl>
-                                <InputGroup>
+                                <InputGroup size="md" >
                                     <InputLeftElement
                                         color="gray.300"><FaUserAlt /></InputLeftElement>
                                     <Input type="email" placeholder="john@doe.com" ref={email} />
                                 </InputGroup>
                             </FormControl>
                             <FormControl>
-                                <InputGroup>
+                                <InputGroup size="md">
                                     <InputLeftElement
                                         color="gray.300"
                                     ><FaLock /></InputLeftElement>
@@ -135,6 +155,6 @@ export default function Login() {
                     </form>
                 </Box>
             </Stack>
-        </Flex>
+        </Flex>}</>
     );
 }
