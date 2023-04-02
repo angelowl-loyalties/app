defd9f799541e4b577dc8b7126b6aedbe2010ce1
omerwyo <omerbaggia123@gmail.com>
2023-04-02 13:10:46 +0800

    revert to non batched insert

--- rewarder/models/reward.go
+++ rewarder/models/reward.go
@@ -1,17 +1,9 @@
 package models
 
 import (
-	"log"
-	"sync"
-	"time"
-
 	"github.com/gocql/gocql"
+	"github.com/scylladb/gocqlx"
 	"github.com/scylladb/gocqlx/qb"
-)
-
-const (
-	MaxBatchSize     = 2000
-	MaxBatchWaitTime = 100 * time.Millisecond
 )
 
 type Reward struct {
@@ -31,124 +23,125 @@ type Reward struct {
 	Remarks         string     `json:"remarks"`          // cassandra text
 }
 
-var (
-	rewardBatch []Reward
-	rewardChan  = make(chan Reward, MaxBatchSize)
-	rewardMutex = &sync.Mutex{}
-	batchTimer  *time.Timer
-)
-
-func init() {
-	batchTimer = time.NewTimer(MaxBatchWaitTime)
-	go batchInsertLoop()
-}
-
-func batchInsertLoop() {
-	for {
-		select {
-		case reward := <-rewardChan:
-			addRewardToBatch(reward)
-		case <-batchTimer.C:
-			flushRewardBatch()
-			resetBatchTimer()
-		}
-	}
-}
-
-func resetBatchTimer() {
-	if !batchTimer.Stop() {
-		<-batchTimer.C
-	}
-	batchTimer.Reset(MaxBatchWaitTime)
-}
-
-func addRewardToBatch(reward Reward) {
-	rewardMutex.Lock()
-	defer rewardMutex.Unlock()
-	rewardBatch = append(rewardBatch, reward)
-
-	if len(rewardBatch) >= MaxBatchSize {
-		flushRewardBatch()
-		resetBatchTimer()
-	}
-}
-
-func flushRewardBatch() {
-	rewardMutex.Lock()
-	defer rewardMutex.Unlock()
-
-	if len(rewardBatch) == 0 {
-		return
-	}
-
-	batch := DB.NewBatch(gocql.LoggedBatch)
-
-	stmt, _ := qb.Insert("angelowl.rewards").Columns(
-		"card_id",
-		"transaction_date",
+func RewardCreate(reward Reward) error {
+	stmt, names := qb.Insert("angelowl.rewards").Columns(
 		"id",
-		"card_pan",
-		"card_type",
-		"amount",
-		"created_at",
-		"currency",
-		"mcc",
+		"card_id",
 		"merchant",
-		"remarks",
-		"reward_amount",
+		"mcc",
+		"currency",
+		"amount",
 		"sgd_amount",
 		"transaction_id",
-	).ToCql()
+		"transaction_date",
+		"created_at",
+		"card_pan",
+		"card_type",
+		"reward_amount",
+		"remarks").ToCql()
 
-	for _, reward := range rewardBatch {
-		batch.Query(stmt, reward.CardID, reward.TransactionDate, reward.ID, reward.CardPAN, reward.CardType,
-			reward.Amount, reward.CreatedAt, reward.Currency, reward.MCC, reward.Merchant, reward.Remarks,
-			reward.RewardAmount, reward.SGDAmount, reward.TransactionID,
-		)
-	}
+	q := gocqlx.Query(DB.Query(stmt), names).BindStruct(&reward)
 
-	err := DB.ExecuteBatch(batch)
+	err := q.ExecRelease()
 	if err != nil {
-		log.Printf("Error executing batch insert: %v", err)
+		return err
 	}
 
-	rewardBatch = rewardBatch[:0]
-}
-
-func RewardCreate(reward Reward) error {
-	rewardChan <- reward
 	return nil
-	// select {
-	// case rewardChan <- &reward:
-	// 	return nil
-	// default:
-	// 	return errors.New("reward channel is full")
-	// }
 }
 
-// func RewardCreate(reward Reward) error {
-// 	stmt, names := qb.Insert("angelowl.rewards").Columns(
-// 		"id",
-// 		"card_id",
-// 		"merchant",
-// 		"mcc",
-// 		"currency",
-// 		"amount",
-// 		"sgd_amount",
-// 		"transaction_id",
-// 		"transaction_date",
-// 		"created_at",
-// 		"card_pan",
-// 		"card_type",
-// 		"reward_amount",
-// 		"remarks").ToCql()
+// Batched Insert code. Not working
 
-// 	q := gocqlx.Query(DB.Query(stmt), names).BindStruct(&reward)
+// const (
+// 	MaxBatchSize     = 2000
+// 	MaxBatchWaitTime = 100 * time.Millisecond
+// )
 
-// 	err := q.ExecRelease()
-// 	if err != nil {
-// 		return err
+// var (
+// 	rewardBatch []Reward
+// 	rewardChan  = make(chan Reward, MaxBatchSize)
+// 	rewardMutex = &sync.Mutex{}
+// 	batchTimer  *time.Timer
+// )
+
+// func init() {
+// 	batchTimer = time.NewTimer(MaxBatchWaitTime)
+// 	go batchInsertLoop()
+// }
+
+// func batchInsertLoop() {
+// 	for {
+// 		select {
+// 		case reward := <-rewardChan:
+// 			addRewardToBatch(reward)
+// 		case <-batchTimer.C:
+// 			flushRewardBatch()
+// 			resetBatchTimer()
+// 		}
+// 	}
+// }
+
+// func resetBatchTimer() {
+// 	if !batchTimer.Stop() {
+// 		<-batchTimer.C
+// 	}
+// 	batchTimer.Reset(MaxBatchWaitTime)
+// }
+
+// func addRewardToBatch(reward Reward) {
+// 	rewardMutex.Lock()
+// 	defer rewardMutex.Unlock()
+// 	rewardBatch = append(rewardBatch, reward)
+
+// 	if len(rewardBatch) >= MaxBatchSize {
+// 		flushRewardBatch()
+// 		resetBatchTimer()
+// 	}
+// }
+
+// func flushRewardBatch() {
+// 	rewardMutex.Lock()
+// 	defer rewardMutex.Unlock()
+
+// 	if len(rewardBatch) == 0 {
+// 		return
 // 	}
 
+// 	batch := DB.NewBatch(gocql.LoggedBatch)
+
+// 	stmt, _ := qb.Insert("angelowl.rewards").Columns(
+// 		"card_id",
+// 		"transaction_date",
+// 		"id",
+// 		"card_pan",
+// 		"card_type",
+// 		"amount",
+// 		"created_at",
+// 		"currency",
+// 		"mcc",
+// 		"merchant",
+// 		"remarks",
+// 		"reward_amount",
+// 		"sgd_amount",
+// 		"transaction_id",
+// 	).ToCql()
+
+// 	for _, reward := range rewardBatch {
+// 		batch.Query(stmt, reward.CardID, reward.TransactionDate, reward.ID, reward.CardPAN, reward.CardType,
+// 			reward.Amount, reward.CreatedAt, reward.Currency, reward.MCC, reward.Merchant, reward.Remarks,
+// 			reward.RewardAmount, reward.SGDAmount, reward.TransactionID,
+// 		)
+// 	}
+
+// 	err := DB.ExecuteBatch(batch)
+// 	if err != nil {
+// 		log.Printf("Error executing batch insert: %v", err)
+// 	}
+
+// 	rewardBatch = rewardBatch[:0]
+// }
+
+// func RewardCreate(reward Reward) error {
+// 	rewardChan <- reward
 // 	return nil
 // }
