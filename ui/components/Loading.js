4f1813f4e2bdd6058b84714f1e14bea6bec0bc54
Nicholas Ong <nicholasongck@gmail.com>
2023-04-04 21:28:16 +0800

    fix: admin login

--- ui/components/Loading.js
+++ ui/components/Loading.js
@@ -1,38 +1,40 @@
-import { Center, Spinner, VStack } from '@chakra-ui/react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-
+import { Center, Spinner, VStack } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 export default function Loading() {
-    const router = useRouter()
+	const router = useRouter();
 
-    const { data: session, status } = useSession({
-        required: true,
-        onUnauthenticated() {
-            router.push("/login")
-        },
-    });
-    useEffect(()=>{
-        if (session && session.is_new){
-            router.push("/changePassword")
-        }
-    },[session])
+	const { data: session, status } = useSession({
+		required: true,
+		onUnauthenticated() {
+			if (router.pathname === "/admin/login") {
+				router.push("/admin/login");
+			} else {
+				router.push("/login");
+			}
+		},
+	});
+	useEffect(() => {
+		if (session && session.is_new) {
+			router.push("/changePassword");
+		}
+	}, [session]);
 
-    return (
-        <Center h="100vh" bg="white">
-            <VStack>
-                <img
-                    src="https://ik.imagekit.io/alvinowyong/g1t2/ascenda.webp"
-                    width="0"
-                    height="0"
-                    sizes="100vw"
-                    style={{ width: "150px", height: "auto", marginBottom: "30px" }}
-                    alt="ascenda logo"
-                />
-                <Spinner size="md" />
-            </VStack>
-        </Center>
-    );
+	return (
+		<Center h="100vh" bg="white">
+			<VStack>
+				<img
+					src="https://ik.imagekit.io/alvinowyong/g1t2/ascenda.webp"
+					width="0"
+					height="0"
+					sizes="100vw"
+					style={{ width: "150px", height: "auto", marginBottom: "30px" }}
+					alt="ascenda logo"
+				/>
+				<Spinner size="md" />
+			</VStack>
+		</Center>
+	);
 }
-
